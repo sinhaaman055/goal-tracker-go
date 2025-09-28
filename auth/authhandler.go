8a95fe663c30314f55e0ffb222af3d0d6199e22a
Aman Sinha <sinhaaman055@gmail.com>
2025-09-28 11:39:35 +0530

    just checking

--- auth/authhandler.go
+++ auth/authhandler.go
@@ -14,7 +14,7 @@ func (h *Handler) SignupHandler(w http.ResponseWriter,r *http.Request){
 }
 func (h *Handler) LoginHandler(w http.ResponseWriter,r *http.Request){
 
-	//abhi code likha jyega guyss
+	//abhi code likha jyega Guyz
 
 }
 func (h *Handler) ForgetPassword(w http.ResponseWriter,r *http.Request){
